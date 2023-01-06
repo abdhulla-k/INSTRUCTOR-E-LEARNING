b49ab1432d1c547147699009eaf84b31a8b489db
abdhulla-k <abdhullak149@gmail.com>
2023-01-06 16:38:59 +0530

    created instructor home, login, signup components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { InstructorHomeComponent } from './instructor-home/instructor-home.component';
 
 const routes: Routes = [
+  {path: 'instructor', component: InstructorHomeComponent},
   {path: '**', component: EmptyRouteComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { InstructorHomeComponent } from './instructor-home/instructor-home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmptyRouteComponent
+    EmptyRouteComponent,
+    InstructorHomeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
