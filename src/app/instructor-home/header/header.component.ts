5f76e36ea535ab3d2820bb4a2d9f4c356b8af470
abdhulla-k <abdhullak149@gmail.com>
2023-01-10 15:12:09 +0530

    solved an error in header component

--- src/app/instructor-home/header/header.component.ts
+++ src/app/instructor-home/header/header.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { ControllerService } from 'src/app/controller.service';
+import * as fromApp from '../../store/app.reducer';
 
 @Component({
   selector: 'app-header',
@@ -13,12 +15,20 @@ export class HeaderComponent implements OnInit {
   faHumbergerMenue = faBars;
 
   constructor(
-    private controllerService: ControllerService
+    private controllerService: ControllerService,
+    private store: Store<fromApp.AppState>
   ) { }
 
-
   ngOnInit(): void {
+    this.store.select('authentication').subscribe(data => {
+      this.loggedIn = data.loggedIn;
+    })
 
+    if(JSON.parse(localStorage.getItem('instructorData') || '{}').jwtToken) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
   }
 
   // function to control side bar with humberger icon
