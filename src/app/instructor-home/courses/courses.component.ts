a4d9e8493801953fb91f6ae4530f587fc0de4960
abdhulla-k <abdhullak149@gmail.com>
2023-01-18 22:27:49 +0530

    made header position fixed and created a type for course

--- src/app/instructor-home/courses/courses.component.ts
+++ src/app/instructor-home/courses/courses.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent {
-
+export class CoursesComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
 }
